b72cae6555eada14778e397b9519d9d082489db1
di2mot <motsar.dm@gmail.com>
2020-05-10 23:59:32 +0300

    upd

--- main.py
+++ main.py
@@ -1,54 +1,66 @@
-import subprocess
-import platform
 import msvcrt
 import os
 import time
 import sys
 
-from field import field
 
 def clear():
     os.system('CLS')
 
 
+hight = 20  # количество строк
+width = 100 # шарина, т.е. сколько символов в строке
+
 ''' Устанавливаем размер игрового поля'''
-os.system("mode con cols=50 lines=16")
+os.system(f"mode con cols={width} lines={hight+1}")
 
 
-def loop():
+
+
+# Цикл
+def loop(hight, width):
+
+    hight_f = 30
+    width_f = 17
+
+    # Поле
+    field = [[ 0 * j for j in range(width)] for i in range(hight)]
+
+
+    field[hight - 1][width - 2] = 1     # устанавливаем врага
+    field[hight - 1][width - 1] = 1  # устанавливаем врага
+    field[hight - 2][width - 1] = 1  # устанавливаем врага
+    field[hight - 2][width - 2] = 1  # устанавливаем врага
+    field[hight - 3][width - 1] = 1  # устанавливаем врага
+    field[hight - 3][width - 2] = 1  # устанавливаем врага
+
+
+
     ''' Рисуем решёлки'''
-    fence = '#' * 50
+    fence = '#' * width
 
-    game = True
     line = 0 # индекс строки
     colum = 0 # индекс столба
     iter = 0 # Считаем циклы
 
     # Главный циклы
-    while game:
+    while True:
 
         # тут я ввёл ограничители по строкам
         line = 5
         # огранчение по количеству символов
         colum += 1
 
+        
+
         # пока что сделал ограничение на количество циклов
         if iter == 50:
-            game = False
-            print(fence)
-
-            print('Game And')
-
-            print(fence)
-
-            input()
+            gameEnd(fence, width)
 
         else:
-            # Рисую заборчик
-            print(fence)
 
-            # функция рабочего поля
-            gameField(line, colum)
+
+            move(field, 19, 49)
 
             # снова заборчик
             print(fence)
@@ -57,58 +69,75 @@ def loop():
             iter += 1
 
             # Функция которая ожидает ввода
-            timed_input('Для управления введи тречолку вверх или вниз')
+            text = 'Для управления введи тречолку вверх или вниз'
+            if timed_input(text) == 'up':
+                # print('Eeeeeeee')
+                time.sleep(5)
 
             # Для очистки терминала
             clear()
-        print(fence)
+            print(fence)
 
-# Функция отвечающая за отображение игровой зоны
-def gameField(l, c):
 
-    hight = 13
-    len = 50
-
-    # Ограничение что бы не выйти а предел
-    if c <= 49:
-        field[l][c] = 0
-        field[l][c-1] = ''
-
-    # Построчно рисуем игровое поле
-    for lines in range(0, hight):
-        temp_line = []
-
-        for i in range(0, len):
-            temp_line.append((field[lines][i]))
-
-        print(*temp_line)
 
 # Функция которая ожидает ввод от пользователя определённое время
 # если пользователь ничего не ввёл за указанное время
 # то ничего не делает
-def timed_input(caption, timeout=0.1):
+def timed_input(caption, timeout=0.5):
     def echo(c):
-        sys.stdout.write(c)
+
         sys.stdout.flush()
 
-    echo(caption)
-
-    _input = []
     start = time.monotonic()
     while time.monotonic() - start < timeout:
         if msvcrt.kbhit():
             c = msvcrt.getwch()
-            if ord(c) == 13:
-                echo('\r\n')
-                break
-            _input.append(c)
-            echo(c)
+            if ord(c) == 32:
+                echo(c)
+                return 'up'
 
-    if _input:
-        return ''.join(_input)
+
+
+def move(field, pos_x, pos_y):
+
+    border_x = 0  # ограничение по х
+    border_y = 4  # ограничение по у
+
+
+    for y in range(0, len(field)):
+        tmp = []
+
+        for x in range(0, len(field[y])):
+            if field[y][x] == 0:
+                tmp.append('')
+
+
+            elif field[y][x] == 1:
+                tmp.append('█')
+                field[y][x] = 0
+                field[y][x-1] = 1
+
+        print(*tmp)
+
+
+
+
+
+def gameEnd(fence, width):
+
+    for i in range(6): print('')
+
+    print('Game and'.center(width))
+
+    for i in range(6): print('')
+    print(fence)
+
+    input('Нажми Enter для выхода')
+    sys.exit()
+
 
 if __name__ == '__main__':
-    loop()
+    loop(hight, width)
 
 
 
