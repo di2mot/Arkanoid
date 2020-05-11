1f958e8282d4fdd640adc5685c4bf948249a0156
di2mot <motsar.dm@gmail.com>
2020-05-11 13:55:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,42 +4,32 @@ import time
 import sys
 
 
-def clear():
-    os.system('CLS')
-
-
-hight = 20  # количество строк
-width = 100 # шарина, т.е. сколько символов в строке
+hight = 20       # количество строк
+width = 100      # шарина, т.е. сколько символов в строке
 
 ''' Устанавливаем размер игрового поля'''
+
 os.system(f"mode con cols={width} lines={hight+1}")
 
-
-
+def clear():
+    os.system('CLS')
 
 # Цикл
 def loop(hight, width):
 
-    hight_f = 30
-    width_f = 17
+    status = ['run', 1]
 
     # Поле
     field = [[ 0 * j for j in range(width)] for i in range(hight)]
 
-
-    field[hight - 1][width - 2] = 1     # устанавливаем врага
-    field[hight - 1][width - 1] = 1  # устанавливаем врага
-    field[hight - 2][width - 1] = 1  # устанавливаем врага
-    field[hight - 2][width - 2] = 1  # устанавливаем врага
-    field[hight - 3][width - 1] = 1  # устанавливаем врага
-    field[hight - 3][width - 2] = 1  # устанавливаем врага
-
+    add_let(field)      # содаём препятсвие
+    add_Dino(field)     # рисум Динозёбра
 
 
     ''' Рисуем решёлки'''
     fence = '#' * width
 
-    line = 0 # индекс строки
+
     colum = 0 # индекс столба
     iter = 0 # Считаем циклы
 
@@ -58,9 +48,7 @@ def loop(hight, width):
             gameEnd(fence, width)
 
         else:
-
-
-            move(field, 19, 49)
+            move(field, status)
 
             # снова заборчик
             print(fence)
@@ -70,9 +58,8 @@ def loop(hight, width):
 
             # Функция которая ожидает ввода
             text = 'Для управления введи тречолку вверх или вниз'
-            if timed_input(text) == 'up':
-                # print('Eeeeeeee')
-                time.sleep(5)
+            timed_input(status)
+
 
             # Для очистки терминала
             clear()
@@ -83,9 +70,8 @@ def loop(hight, width):
 # Функция которая ожидает ввод от пользователя определённое время
 # если пользователь ничего не ввёл за указанное время
 # то ничего не делает
-def timed_input(caption, timeout=0.5):
-    def echo(c):
-
+def timed_input(status, timeout=0.5):
+    def echo():
         sys.stdout.flush()
 
     start = time.monotonic()
@@ -93,12 +79,18 @@ def timed_input(caption, timeout=0.5):
         if msvcrt.kbhit():
             c = msvcrt.getwch()
             if ord(c) == 32:
-                echo(c)
-                return 'up'
+                echo()
+                status[0] = 'up'
 
 
 
-def move(field, pos_x, pos_y):
+def move(field, status):
+
+    '''
+    :param field: - параметры игрового поля
+    :param status: - сообщает о небходимом действии: 'u'p и 'run'
+    :return:
+    '''
 
     border_x = 0  # ограничение по х
     border_y = 4  # ограничение по у
@@ -107,19 +99,87 @@ def move(field, pos_x, pos_y):
     for y in range(0, len(field)):
         tmp = []
 
-        for x in range(0, len(field[y])):
-            if field[y][x] == 0:
-                tmp.append('')
+        dino = {10: '/', 11: '#',  12: '@', 13: '\\', 14: '-', 15: '=', 16: '|'}
 
+        for x in range(0, len(field[y])):
+
+            # if field[y][x] >= 10: # проверяем на условие проигрыша
+            #     gameEnd('#'*50, 50)
+
+            if status[0] == 'run':
+                if field[y][x] in dino:
+                    tmp.append(dino[field[y][x]])
+
+            elif status[0] == 'up':
+                if field[y-1][x] in dino:
+                    tmp.append(dino[field[y-1][x]])
+                    field[y - 1][x] = 0
+
+
+
+            if field[y][x] == 0:
+                tmp.append('.')
 
             elif field[y][x] == 1:
                 tmp.append('█')
                 field[y][x] = 0
                 field[y][x-1] = 1
 
+
+
+
+
         print(*tmp)
+    status[0] = 'run'
 
 
+def add_let(field):
+    ''' Препятсвия'''
+    field[hight - 1][width - 2] = 1  # устанавливаем врага
+    field[hight - 1][width - 1] = 1  # устанавливаем врага
+    field[hight - 2][width - 1] = 1  # устанавливаем врага
+    field[hight - 2][width - 2] = 1  # устанавливаем врага
+    field[hight - 3][width - 1] = 1  # устанавливаем врага
+    field[hight - 3][width - 2] = 1  # устанавливаем врага
+
+def add_Dino(field):
+
+    '''
+...2345
+-5 /#@\
+-4 /#-/
+-3 /#==
+-2 /|\\
+-1 /|\\
+
+10: '/', 11: '#',  12: '@', 13: '\\', 14: '-', 15: '=', 16: '||'
+
+    '''
+
+    field[hight - 1][2] = 10  # устанавливаем Dino
+    field[hight - 1][3] = 10
+    field[hight - 1][4] = 16  # устанавливаем Dino
+    field[hight - 1][5] = 16  # устанавливаем Dino
+
+    field[hight - 2][2] = 10  # устанавливаем Dino
+    field[hight - 2][3] = 10  # устанавливаем Dino
+    field[hight - 2][4] = 13  # устанавливаем Dino
+    field[hight - 2][5] = 13  # устанавливаем Dino
+
+    field[hight - 3][2] = 10  # устанавливаем Dino
+    field[hight - 3][3] = 11  # устанавливаем Dino
+    field[hight - 3][4] = 15  # устанавливаем Dino
+    field[hight - 3][5] = 15  # устанавливаем Dino
+
+    field[hight - 4][2] = 10  # устанавливаем Dino
+    field[hight - 4][3] = 11  # устанавливаем Dino
+    field[hight - 4][4] = 14  # устанавливаем Dino
+    field[hight - 4][5] = 10  # устанавливаем Dino
+
+    field[hight - 5][2] = 10  # устанавливаем Dino
+    field[hight - 5][3] = 11  # устанавливаем Dino
+    field[hight - 5][4] = 12  # устанавливаем Dino
+    field[hight - 5][5] = 13  # устанавливаем Dino
 
 
 
